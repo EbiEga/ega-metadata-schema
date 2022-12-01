156e94787a6c52641fa4df2b2f6db948756ddbd0
M-casado <mcasado@ebi.ac.uk>
2022-12-01 13:25:09 +0000

    EE-2575 - Again following the linter demands

--- .github/scripts/request_validation.py
+++ .github/scripts/request_validation.py
@@ -18,14 +18,14 @@ extension = ".json"
 
 
 def request_validation(
-        data_filepath: tp.Union[str, os.DirEntry[str]], curl_URL: str, headers: dict = None
-    ) -> requests.models.Response:
+    data_filepath: tp.Union[str, os.DirEntry[str]], curl_URL: str, headers: dict = None
+) -> requests.models.Response:
     """
     Function that, given a data_filepath (e.g. "path/to/file.json"), a URL (e.g. http://localhost:3020/validate)
         and the HTTP headers, will do a post request and return the response
     """
     if headers is None:
-        headers = {"Content-Type': 'application/json"}
+        headers = {"Content-Type": "application/json"}
 
     with open(data_filepath) as f:
         data = f.read().replace("\n", "").replace("\r", "").encode("utf-8")
@@ -34,7 +34,7 @@ def request_validation(
     return response
 
 
-def get_errors_response(response: requests.models.Response) -> tp.Union[list,str]:
+def get_errors_response(response: requests.models.Response) -> tp.Union[list, str]:
     """
     Function that, given a "requests.models.Response" object, will interpret it, asserting that the request
         was successful and no validation errors (i.e. empty list of errors) were found. If not, it will
@@ -47,9 +47,9 @@ def get_errors_response(response: requests.models.Response) -> tp.Union[list,str
 
     if not response.status_code == requests.codes.ok:
         error_message = (
-                    f"The POST response was not successful: instead of {requests.codes.ok},"
-                    + f" the status code was '{response.status_code}' when validating file '{filename}'"
-                )
+            f"The POST response was not successful: instead of {requests.codes.ok},"
+            f" the status code was '{response.status_code}' when validating file '{filename}'"
+        )
         return error_message
 
     val_response_list = response.json()
@@ -57,6 +57,8 @@ def get_errors_response(response: requests.models.Response) -> tp.Union[list,str
     # If the list is empty "[]", the validation found no errors
     if not len(val_response_list) == 0:
         return val_response_list
+    else:
+        return
 
 
 # --------- #
